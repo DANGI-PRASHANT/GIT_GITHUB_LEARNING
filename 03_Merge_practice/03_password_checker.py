d6ae6e86efe882ffb7a92f8e37a3f4ae32a974b5
8NOXX <dangiprashant009@gmail.com>
2026-09-10 20:55:25 -0700

    password checker

---
+++ 03_Merge_practice/03_password_checker.py
@@ -0,0 +1,10 @@
+while True:
+
+    password = " "
+
+    if password != "Ram@123":
+        password = input("Enter a password: ")
+        print("Invalid username or password , please try again.")
+    else:
+        print("Login succesfull")
+
