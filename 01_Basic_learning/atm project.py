d85d742f4f5c218e77ad4ea6cf088aa7b79fff90
8NOXX <dangiprashant009@gmail.com>
2026-09-10 18:16:27 -0700

    Update

--- 01_Basic_learning/atm project.py
+++ 01_Basic_learning/atm project.py
@@ -1,4 +1,3 @@
-
 card = True
 balance = 500
 
@@ -11,5 +10,14 @@ if pin == 1234:
     if option == "Balance check":
         print(f"Your Balance is {balance} $.")
 
-    
+       
+    elif option == "withdraw":
+
+            withdraw = int(input("Enter amount: "))
+
+            if withdraw >= balance:
+                print("Insufficent Balance")
+            else:
+                print("Withdraw Successful")
+                print(f"Remaining amount is {balance - withdraw} $")
 
