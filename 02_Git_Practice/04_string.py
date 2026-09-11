d91895e7c3f08ce6772880a599d8357326e9c6d8
8NOXX <dangiprashant009@gmail.com>
2026-09-10 20:20:34 -0700

    string

---
+++ 02_Git_Practice/04_string.py
@@ -0,0 +1,8 @@
+name = input("Enter your name: ")
+age  = int(input("Enter your age: "))
+location = input("Enter your location: ")
+
+print()
+print(f"My name is {name}")
+print(f"i am {age} years old.")
+print(f"My location is {location}") 
