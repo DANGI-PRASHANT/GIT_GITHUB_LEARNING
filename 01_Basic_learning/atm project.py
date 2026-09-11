abe55ca9df175ce630226e030bf55d69a22cf8e6
8NOXX <dangiprashant009@gmail.com>
2026-09-10 18:02:37 -0700

    Update

---
+++ 01_Basic_learning/atm project.py
@@ -0,0 +1,15 @@
+
+card = True
+balance = 500
+
+pin = 1234
+
+if pin == 1234:
+
+    option = input("(Balance check) / (Withdraw) / (Deposit) : " )
+
+    if option == "Balance check":
+        print(f"Your Balance is {balance} $.")
+
+    
+
