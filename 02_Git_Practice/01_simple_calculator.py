dccdd4edaaea4e85c07b2f50599fc9bbb83b0737
8NOXX <dangiprashant009@gmail.com>
2026-09-10 19:34:06 -0700

    add a features

--- 02_Git_Practice/01_simple_calculator.py
+++ 02_Git_Practice/01_simple_calculator.py
@@ -6,3 +6,10 @@ print(f"Sum of two number : {num1 + num2}")
 print(f"Sub of two number : {num1 -num2}")
 print(f"Multiple of two number: {num1 * num2}")
 print(f"Divided : {num1 / num2}")
+print()
+
+# add-square-feature:
+
+num = float(input("Enter a number: "))
+
+print(f"square : {num **2}")
